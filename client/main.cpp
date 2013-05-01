d09da49a6063b747ff7b46eef8493c9d633b6562
spektre <bei.oden@gmail.com>
2013-05-02 00:10:48 +0200

    Added client/main.cpp to commit

--- client/main.cpp
+++ client/main.cpp
@@ -1,21 +1,18 @@
-#include <sys/socket.h>
 #include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+#include "platforms.h"
+
 #define BUFLEN 256
 
 int main(int argc, char *argv[]) {
-    int s;
     char buf[BUFLEN] = {};
     sockaddr_in si_remote = {};
-    socklen_t socklen = sizeof(si_remote);
 
-    s = socket(AF_INET, SOCK_DGRAM, 0);
-    if (s < 0) {
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
         perror("Could not create socket");
         exit(1);
     }
@@ -31,7 +28,7 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < 10; i++) {
         printf("Sending packet\n");
         int len = strlen(buf);
-        r = sendto(s, buf, len, 0, (sockaddr*)&si_remote, socklen);
+        r = sendto(sockfd, buf, len, 0, (sockaddr*)&si_remote, socklen);
         if (r == -1) {
             perror("sendto()");
             exit(1);
